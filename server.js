aad6fe870e21873f1971c5d3f522ee3ac9ee098e
Guido <guidoquayat@gmail.com>
2018-06-18 14:07:27 -0300

    test con filter apply

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ function test() { ffmpeg('rtmp://127.0.0.1:1935/mytv/test')
   // .addOption('preset','superfast')
   // set target codec
   .videoCodec('libx264')
+  .applyFilters('eof_action')
   // set audio bitrate
   .audioBitrate('128k')
   // set audio codec
@@ -52,7 +53,7 @@ function test() { ffmpeg('rtmp://127.0.0.1:1935/mytv/test')
   .audioChannels(2)
   // set hls segments time
   .addOption('-f flv')
-  .applyFilters('eof_action')
+
 
 
 
