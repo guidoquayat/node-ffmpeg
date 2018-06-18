6875c51e352e57feeb39eb4062b5ca0c805ad494
Guido <guidoquayat@gmail.com>
2018-06-18 14:45:05 -0300

    Patada voladora 2

--- server.js
+++ server.js
@@ -39,18 +39,27 @@ function test() { ffmpeg('rtmp://127.0.0.1:1935/mytv/test')
   // set video bitrate
   .videoBitrate(1024)
 
-   .inputOption('-fflags +igndts')
+   // .inputOption('-fflags +igndts')
   // set h264 preset
   // .addOption('preset','superfast')
   // set target codec
   .videoCodec('libx264')
   // set audio bitrate
-  .audioBitrate('128k')
+  // .audioBitrate('128k')
   // set audio codec
   // .inputOption('-listen 1')
   // set number of audio channels
-  .audioChannels(2)
+  // .audioChannels(2)
   // set hls segments time
+  .addOption('-pix_fmt yuv420p')
+  .addOption('-preset ultrafast')
+  .addOption('-r 25')
+  .addOption('-g 20 ')
+  .addOption('-b:v 2500k')
+  .addOption('-codec:a libmp3lame')
+  .addOption('-ar 44100')
+  .addOption('-b:a 11025')
+  .addOption('-bufsize 512k')
   .addOption('-f flv')
   // .addOption('-filter: eof_action')
 
